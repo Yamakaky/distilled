788a86b73c59f92e00d767da1a0182f23933d569
Yamakaky <yamakaky@yamaworld.fr>
2020-10-06 21:15:02 +0200

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -8,34 +8,96 @@ fn main() -> Result<()> {
     let wasm_bytes = include_bytes!("../target/wasm32-unknown-unknown/debug/wasm.wasm");
     let param: String = Param { a: 1, b: 2 }.serialize_json();
 
-    let ret = runner.run(wasm_bytes, "add", param.as_bytes())?;
+    let ret = runner.run(wasm_bytes.to_vec(), "add".into(), param.into_bytes())?;
 
     let result = {
         let mut state = types::nanoserde::DeJsonState::default();
         let mut chars = std::str::from_utf8(&ret)?.chars();
         state.next(&mut chars);
         state.next_tok(&mut chars).context("return value deser")?;
-        Ret::de_json(&mut state, &mut chars).context("return value deser")?.ret
+        Ret::de_json(&mut state, &mut chars)
+            .context("return value deser")?
+            .ret
     };
     println!("Result: {:?}", result);
 
     Ok(())
 }
 
+enum Req {
+    Run {
+        id: u64,
+        wasm: Vec<u8>,
+        func: String,
+        params: Vec<u8>,
+    },
+    Stop,
+}
+
+enum Res {
+    Result { id: u64, res: Vec<u8> },
+}
+
 #[derive(Debug, Clone)]
 struct Runner {
     store: wasmer::Store,
+    req_queue: crossbeam_channel::Sender<Req>,
+    res_queue: crossbeam_channel::Receiver<Res>,
 }
 
 impl Runner {
     fn new() -> Self {
+        let store = Store::new(&JIT::new(&Cranelift::default()).engine());
+        let (req_queue, worker_req) = crossbeam_channel::unbounded();
+        let (worker_res, res_queue) = crossbeam_channel::unbounded();
+        for _ in 0..4 {
+            let worker_req = worker_req.clone();
+            let worker_res = worker_res.clone();
+            let store = store.clone();
+            std::thread::spawn(move || loop {
+                match worker_req.recv() {
+                    Ok(Req::Run {
+                        id,
+                        wasm,
+                        func,
+                        params,
+                    }) => {
+                        let _ = worker_res.send(Res::Result {
+                            id,
+                            res: Runner::job(&store, &wasm, &func, &params).unwrap(),
+                        });
+                    }
+                    Ok(Req::Stop) | Err(crossbeam_channel::RecvError) => break,
+                }
+            });
+        }
         Self {
-            store: Store::new(&JIT::new(&Cranelift::default()).engine()),
+            store,
+            req_queue,
+            res_queue,
         }
     }
 
-    fn run(&self, wasm_bytes: &[u8], func_name: &str, param_bytes: &[u8]) -> Result<Vec<u8>> {
-        let module = Module::new(&self.store, wasm_bytes).context("module compilation")?;
+    pub fn run(&self, wasm: Vec<u8>, func: String, params: Vec<u8>) -> Result<Vec<u8>> {
+        let rid = 1;
+        self.req_queue.send(Req::Run {
+            id: rid,
+            wasm,
+            func,
+            params,
+        })?;
+        let Res::Result { id, res } = self.res_queue.recv()?;
+        assert_eq!(id, rid);
+        Ok(res)
+    }
+
+    fn job(
+        store: &Store,
+        wasm_bytes: &[u8],
+        func_name: &str,
+        param_bytes: &[u8],
+    ) -> Result<Vec<u8>> {
+        let module = Module::new(store, wasm_bytes).context("module compilation")?;
         let import_object = imports! {};
         let instance = Instance::new(&module, &import_object).context("module instanciation")?;
         let wasm_memory = instance.exports.get_memory("memory").expect("wasm memory");
@@ -68,5 +130,13 @@ impl Runner {
             .iter()
             .map(std::cell::Cell::get)
             .collect())
+    }
+}
+
+impl Drop for Runner {
+    fn drop(&mut self) {
+        for _ in 0..4 {
+            self.req_queue.send(Req::Stop).unwrap();
+        }
     }
 }
